f963cfa60e0de3385de3795709662d79540abf48
WillBeBetterQ <tuxto2000@mail.ru>
2021-05-24 14:01:01 +0300

    odin

---
+++ src/main/java/com/example/RestfulTask/RestfulTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestfulTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulTaskApplication.class, args);
+	}
+
+}
